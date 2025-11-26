c8c20c20bcca0d500998ad2a9c6c3b5a406a5043
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-11-27 01:39:44 +0700

    very very very minor change

--- levels/level_1/level_1.gd
+++ levels/level_1/level_1.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var my_logger: Logger = NoLogger.new()
+var my_logger: Logger = ConsoleLogger.new()
 
 func _enter_tree() -> void:
 	# Handle portal spawning first
@@ -11,5 +11,5 @@ func _ready() -> void:
 		GameManager.respawn_at_checkpoint()
 	
 	GameManager.logger.log("Hi Im global logger, Im from level 1")
-	my_logger.log(("Hi Im script-level logger, Im from level 1"))
+	my_logger.log("Hi Im script-level logger, Im from level 1")
 	
