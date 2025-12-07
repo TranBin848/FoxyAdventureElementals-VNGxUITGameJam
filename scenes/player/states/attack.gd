afec6b0efd18a7034d11377b37dca7e57446f626
kllp031 <31173206+kllp031@users.noreply.github.com>
2025-12-07 14:34:04 +0700

    Attack animation timing now match hit hox timing

--- scenes/player/states/attack.gd
+++ scenes/player/states/attack.gd
@@ -4,19 +4,24 @@ func _enter() -> void:
 	#Change animation to attack
 	obj.change_animation("attack")
 	AudioPlayer.play_sound_once(obj.attack_sfx)
-	timer = 0.2
 	obj.velocity.x = 0
+	
 	#Enable collision shape of hit area
 	obj.get_node("Direction/HitArea2D/CollisionShape2D").disabled = false
+	
+	#add fx
 	var slash_fx = obj.slash_fx_factory.create() as Node2D
 	slash_fx.scale.x = -obj.direction
+	
+	await obj.animated_sprite.animation_finished
+	change_state(fsm.previous_state)
 
 func _exit() -> void:
 	#Disable collision shape of hit area
 	obj.get_node("Direction/HitArea2D/CollisionShape2D").disabled = true
 	obj.start_atk_cd()
 
-func _update(delta: float) -> void:
-	#If attack is finished change to previous state
-	if update_timer(delta):
-		change_state(fsm.previous_state)
+#func _update(delta: float) -> void:
+	##If attack is finished change to previous state
+	#if update_timer(delta):
+		#change_state(fsm.previous_state)
