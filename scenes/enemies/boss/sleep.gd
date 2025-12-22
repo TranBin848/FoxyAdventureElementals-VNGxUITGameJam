9662dc62a51f9eaac22f56b2b8d6752afe8916e3
kllp031 <31173206+kllp031@users.noreply.github.com>
2025-12-22 17:46:46 +0700

    boss hit box is turn before player enter boss zone

--- scenes/enemies/boss/sleep.gd
+++ scenes/enemies/boss/sleep.gd
@@ -4,6 +4,8 @@ func _enter() -> void:
 	obj.change_animation("inactive")
 	obj.velocity.x = 0
 	timer = 0
+	$"../../Direction/HitArea2D/CollisionShape2D".disabled = true
+	$"../../Direction/HurtArea2D/CollisionShape2D2".disabled = true
 
 func _update(_delta: float) -> void:
 	#dont do anything until player is found
@@ -11,3 +13,7 @@ func _update(_delta: float) -> void:
 		return
 		
 	fsm.change_state(fsm.states.standup)
+
+func _exit() -> void:
+	$"../../Direction/HitArea2D/CollisionShape2D".disabled = false
+	$"../../Direction/HurtArea2D/CollisionShape2D2".disabled = false
