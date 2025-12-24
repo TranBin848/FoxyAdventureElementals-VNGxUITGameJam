e2bd354e0b7a71470efb35774af4dd11307811a1
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-12-24 15:57:55 +0700

    add new lever sprites to lever scene

--- scenes/objects/house_door/house_door.gd
+++ scenes/objects/house_door/house_door.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name HouseDoor
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $StaticBody2D/CollisionShape2D
@@ -8,7 +9,7 @@ var logger: Logger = ConsoleLogger.new()
 func _ready():
 	animated_sprite.play("close")
 	collision_shape.disabled = false
-
+	
 func open_door():
 	animated_sprite.play("open")
 	collision_shape.disabled = true
