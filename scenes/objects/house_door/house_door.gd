39e5fd46485b01d31def132635af7d27374b0ca2
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-12-28 02:23:32 +0700

    design: done wood area frame for level 5

--- scenes/objects/house_door/house_door.gd
+++ scenes/objects/house_door/house_door.gd
@@ -4,9 +4,15 @@ class_name HouseDoor
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $StaticBody2D/CollisionShape2D
 
+@export var is_open: bool
+
 var logger: Logger = ConsoleLogger.new()
 
 func _ready():
+	if is_open:
+		animated_sprite.play("open")
+		collision_shape.disabled = true
+		return
 	animated_sprite.play("close")
 	collision_shape.disabled = false
 	
