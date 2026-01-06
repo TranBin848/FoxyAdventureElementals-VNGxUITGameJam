8c1523bb510836cc916d53a41ed40abc15ed8e57
Searching96 <nguyenphucthinh2k5@gmail.com>
2026-01-06 13:47:24 +0700

    change

--- levels/level_2/level_2.gd
+++ levels/level_2/level_2.gd
@@ -1,23 +1,7 @@
 extends Node
 
-func _enter_tree() -> void:
-	# Handle portal spawning first
-	#GameManager.current_stage = self
-	#GameManager.minimap = find_child("Minimap")
-	pass
-	
 func _ready() -> void:
 	GameManager.current_level = 2
 	if AudioManager:
 		AudioManager.play_music("music_background")
-		AudioManager.play_ambience("ambience_forest")
-
-	#var skill_names = ["Thousand Swords", "Wooden Clone", "Earthquake", "Tornado"]
-	#
-	#for i in skill_names.size():
-		## 🔥 Validate + equip
-		#var skill = SkillDatabase.get_skill_by_name(skill_names[i])
-		#if skill:
-			#SkillTreeManager.unlock_skill(skill_names[i],0)
-			#SkillTreeManager.equip_skill(i, skill_names[i])
-			#print("✅ Equipped %s to slot %d" % [skill_names[i], i])
+		AudioManager.play_ambience("ambience_forest", -10)
--- levels/level_6/level_6.gd
+++ levels/level_6/level_6.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready() -> void:
-	GameManager.current_level = 1
+	GameManager.current_level = 6
 	if AudioManager:
 		AudioManager.play_music("music_background")
 		AudioManager.play_ambience("ambience_forest", -10)
