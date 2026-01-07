0fd41ce4807ca0804fb548ad7024f4c7d6abd7f0
NguyenThanhVu028 <23521811@gm.uit.edu.vn>
2026-01-08 01:21:16 +0700

    Improve spawner
    Spawner only spawns when player is within a specific area

--- Scenes/enemies/spawner/idle.gd
+++ Scenes/enemies/spawner/idle.gd
@@ -4,5 +4,5 @@ func _enter() -> void:
 	obj.change_animation("idle")
 
 func _update(_delta: float) -> void:
-	if obj.update_spawn_timer(_delta):
+	if obj.update_spawn_timer(_delta) && obj.found_player != null:
 		change_state(fsm.states.spawn)
